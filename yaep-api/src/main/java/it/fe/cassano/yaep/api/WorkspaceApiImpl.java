8e7b96628345a69939d08d30e7663f2f8f98819d
spetix <christian@cassano.fe.it>
2022-09-01 01:19:12 +0200

    api compiles

---
+++ yaep-api/src/main/java/it/fe/cassano/yaep/api/WorkspaceApiImpl.java
@@ -0,0 +1,34 @@
+package it.fe.cassano.yaep.api;
+
+
+import java.util.List;
+
+import it.fe.cassano.yaep.model.WorkspaceDef;
+
+
+public class WorkspaceApiImpl implements WorkspaceApi {
+
+    @Override
+    public String createWorkspace(String workspaceID, Boolean workspacePersistent, WorkspaceDef workspaceDef) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteWorkspace(String workspaceID) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public WorkspaceDef getWorkspace(String workspaceID) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<String> listWorkspaces() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+} 
