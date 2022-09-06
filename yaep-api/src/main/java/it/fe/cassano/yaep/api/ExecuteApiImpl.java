71230ad81c7d949ba190a254f12bdc4218bb19e2
spetix <christian@cassano.fe.it>
2022-09-06 02:10:21 +0200

    added skeleton

---
+++ yaep-api/src/main/java/it/fe/cassano/yaep/api/ExecuteApiImpl.java
@@ -0,0 +1,13 @@
+package it.fe.cassano.yaep.api;
+
+public class ExecuteApiImpl {
+
+    
+    java.util.LinkedList<Object> executeWorkspaceIDPost(final String workspaceID,final java.util.LinkedList<String> requestBody) {
+        return null;
+    }
+
+    ExecuteApiImpl() {
+        
+    }
+}
